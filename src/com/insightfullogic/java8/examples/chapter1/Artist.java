7b10446ffb51abc166fee54ba9160245b5d553b9
jxd <jinxd134@foxmail.com>
2016-06-30 12:43:37 +0800

    add some example class

---
+++ src/com/insightfullogic/java8/examples/chapter1/Artist.java
@@ -0,0 +1,11 @@
+package com.insightfullogic.java8.examples.chapter1;
+
+import java.util.List;
+
+public final class Artist {
+	
+	private String name;
+	private List<Artist> members;
+	private String nationality;
+	
+}
