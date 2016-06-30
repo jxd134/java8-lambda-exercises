61521bce6803ad00004c1721098e8192254b3959
jxd <jinxd134@foxmail.com>
2016-06-30 12:48:52 +0800

    add construct for Artist

--- src/com/insightfullogic/java8/examples/chapter1/Artist.java
+++ src/com/insightfullogic/java8/examples/chapter1/Artist.java
@@ -1,11 +1,26 @@
 package com.insightfullogic.java8.examples.chapter1;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public final class Artist {
-	
+
 	private String name;
 	private List<Artist> members;
 	private String nationality;
-	
+
+	public Artist(String name, String nationality) {
+		this(name, Collections.emptyList(), nationality);
+	}
+
+	public Artist(String name, List<Artist> members, String nationality) {
+		Objects.requireNonNull(name);
+		Objects.requireNonNull(members);
+		Objects.requireNonNull(nationality);
+		this.name = name;
+		this.members = new ArrayList<>(members);
+		this.nationality = nationality;
+	}
 }
