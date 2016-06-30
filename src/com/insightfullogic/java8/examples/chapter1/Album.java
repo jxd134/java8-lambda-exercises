2e7716aa3f86edb68e8bce007c2b7b318c5fba65
jxd <jinxd134@foxmail.com>
2016-06-30 18:32:37 +0800

    complete Album class

--- src/com/insightfullogic/java8/examples/chapter1/Album.java
+++ src/com/insightfullogic/java8/examples/chapter1/Album.java
@@ -2,6 +2,11 @@ package com.insightfullogic.java8.examples.chapter1;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
 
 public final class Album implements Performance {
 	
@@ -19,5 +24,33 @@ public final class Album implements Performance {
 		this.musicians = musicians;
 	}
 	
+	public String getName(){
+		return name;
+	}
 	
+	public Stream<Track> getTracks() {
+		return tracks.stream();
+	}
+	
+	public List<Track> getTrackList(){
+		return unmodifiableList(tracks);
+	}
+	
+	public Stream<Artist> getMusicians(){
+		return musicians.stream();
+	}
+	
+	public List<Artist> getMusiciansList() {
+		return unmodifiableList(musicians);
+	}
+	
+	public Artist getMusician(){
+		return musicians.get(0);
+	}
+	
+	public Album copy(){
+		List<Track> tracks=getTracks().map(Track::copy())getClass().collect(toList());
+		List<Artist> musicians=getMusician().map(Artist::copy()).collector(toList());
+		return new Album(name, tracks, musicians);
+	}
 }
