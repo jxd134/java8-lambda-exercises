ede517c4592733fd415385a47b99e140c4682e46
jxd <jinxd134@foxmail.com>
2016-07-05 00:35:36 +0800

    finish question1 in chapter3

--- src/com/insightfullogic/java8/answers/chapter1/Question1.java
+++ src/com/insightfullogic/java8/answers/chapter1/Question1.java
@@ -3,6 +3,7 @@ package com.insightfullogic.java8.answers.chapter1;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.Artist;
 
 import static java.util.stream.Collectors.toList;
@@ -14,10 +15,14 @@ public class Question1 {
 	}
 
 	public static List<String> getNamesAndOriginsOfArtists(List<Artist> artists) {
-		return artists.stream().
-				flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
+		return artists.stream()
+				.flatMap(artist -> Stream.of(artist.getName(), artist.getNationality()))
 				.collect(toList());
 	}
-	
-	
+
+	public static List<Album> getAlbumsWithAtMostThreeTracks(List<Album> albums) {
+		return albums.stream()
+				.filter(album -> album.getTrackList().size() <= 3)
+				.collect(toList());
+	}
 }
