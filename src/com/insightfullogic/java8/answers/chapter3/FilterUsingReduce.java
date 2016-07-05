e149bc3c6cdf4ca5a52724a8901cf3976e09f05f
jxd <jinxd134@foxmail.com>
2016-07-05 22:08:06 +0800

    complete FilterUsingReduce answer for chapter3

--- src/com/insightfullogic/java8/answers/chapter3/FilterUsingReduce.java
+++ src/com/insightfullogic/java8/answers/chapter3/FilterUsingReduce.java
@@ -6,27 +6,25 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class FilterUsingReduce {
-	
-	public static <I> List<I> filter(Stream<I> stream,Predicate<I> predicate){
-	
-		List<I> initial = new ArrayList<>();
-		return stream.reduce(initial,
-							(acc,x)->{
-								if(predicate.test(x)){
-									List<I> newAcc=new ArrayList<I>(acc);
-									newAcc.add(x);
-									return newAcc;
-								}else{
-									return acc;
-								}
-							}
-						},FilterUsingReduce::combineLists);
-	}
 
-
-	private static <I> List<I> combinLists(List<I> left,List<I> right){
-		List<I> newLeft=new ArrayList<>(left);
+	private static <I> List<I> combineLists(List<I> left, List<I> right) {
+		List<I> newLeft = new ArrayList<>(left);
 		newLeft.addAll(right);
 		return newLeft;
+	}
+
+	//基本思路与MapUsingReduce类似,参考MapUsingReduce注释
+	public static <I> List<I> filter(Stream<I> stream, Predicate<I> predicate) {
+
+		List<I> initial = new ArrayList<>();
+		return stream.reduce(initial, (List<I> acc, I x) -> {
+			if (predicate.test(x)) {
+				List<I> newAcc = new ArrayList<>(acc);
+				newAcc.add(x);
+				return newAcc;
+			} else {
+				return acc;
+			}
+		}, FilterUsingReduce::combineLists);
 	}
 }
