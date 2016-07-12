859169fb606af1cf89aab3aa3cb41221aa147ea7
jxd <jinxd134@foxmail.com>
2016-07-12 18:22:28 +0800

    complete test for question1 in chapter3

--- src/com/insightfullogic/java8/test/chapter3/Question1Test.java
+++ src/com/insightfullogic/java8/test/chapter3/Question1Test.java
@@ -1,11 +1,16 @@
 package com.insightfullogic.java8.test.chapter3;
 
 import org.junit.Test;
+
+import java.util.List;
 import java.util.stream.Stream;
 
 import com.insightfullogic.java8.answers.chapter3.*;
+import com.insightfullogic.java8.examples.chapter1.Album;
+import com.insightfullogic.java8.examples.chapter1.SampleData;
 
 import static org.junit.Assert.*;
+import static java.util.Arrays.asList;
 
 public class Question1Test {
 
@@ -21,4 +26,16 @@ public class Question1Test {
 		assertEquals(2, result);
 	}
 
+	@Test
+	public void testGetNamesAndOriginsOfArtists() {
+		List<String> namesAndOriginsOfArtists = Question1.getNamesAndOriginsOfArtists(SampleData.getThreeAtrists());
+		assertEquals(asList("John Coltrane", "US", "John Lennon", "UK", "The Beatles", "UK"), namesAndOriginsOfArtists);
+	}
+
+	@Test
+	public void testFindShortAlbums() {
+		List<Album> input = asList(SampleData.manyTrackAlbum, SampleData.sampleShortAlbum, SampleData.aLoveSupreme);
+		List<Album> result = Question1.getAlbumsWithAtMostThreeTracks(input);
+		assertEquals(asList(SampleData.sampleShortAlbum, SampleData.aLoveSupreme), result);
+	}
 }
