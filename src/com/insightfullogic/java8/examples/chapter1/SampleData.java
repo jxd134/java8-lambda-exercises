2412cd7153bb6cb9b51c48baf78ff6269a2d0293
jxd <jinxd134@foxmail.com>
2016-07-12 12:56:22 +0800

    complete the sampleData

--- src/com/insightfullogic/java8/examples/chapter1/SampleData.java
+++ src/com/insightfullogic/java8/examples/chapter1/SampleData.java
@@ -2,14 +2,17 @@ package com.insightfullogic.java8.examples.chapter1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
 
 public class SampleData {
 
 	public static final Artist johnColtrane = new Artist("John Coltrane", "US");
 
-	//TheBeatles
+	// TheBeatles
 	public static final Artist johnLennon = new Artist("John Lennon", "UK");
 	public static final Artist paulMcCartney = new Artist("Paul McCartney", "UK");
 	public static final Artist georgeHarrison = new Artist("George Harrison", "UK");
@@ -17,8 +20,26 @@ public class SampleData {
 
 	public static final List<Artist> membersOfTheBeatles = Arrays.asList(johnLennon, paulMcCartney, georgeHarrison,
 			ringoStarr);
-	
-	public static final Artist theBeatles=new Artist("The Beatles", membersOfTheBeatles,"UK");
-	
-	public static final Album aLoveSupreme=new Album("A Love Supreme", asList(new Track("Acknowledgement", 467), new Track("Resolution", 442)), asList(johnColtrane));
+
+	public static final Artist theBeatles = new Artist("The Beatles", membersOfTheBeatles, "UK");
+
+	// 使用asList省略了转换步骤
+	public static final Album aLoveSupreme = new Album("A Love Supreme",
+			asList(new Track("Acknowledgement", 467), new Track("Resolution", 442)), asList(johnColtrane));
+	public static final Album sampleShortAlbum = new Album("sample Short Album", asList(new Track("short track", 30)),
+			asList(johnColtrane));
+	public static final Album manyTrackAlbum = new Album(
+			"sample Short Album", asList(new Track("short track", 30), new Track("short track 2", 30),
+					new Track("short track 3", 30), new Track("short track 4", 30), new Track("short track 5", 30)),
+			asList(johnColtrane));
+
+	public static Stream<Album> albums = Stream.of(aLoveSupreme);
+
+	public static Stream<Artist> threeArtists() {
+		return Stream.of(johnColtrane, johnLennon, theBeatles);
+	}
+
+	public static List<Artist> getThreeAtrists() {
+		return Arrays.asList(johnColtrane, johnLennon, theBeatles);
+	}
 }
