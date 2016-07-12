1354424fc80867ee2dafe73a5122072040650076
jxd <jinxd134@foxmail.com>
2016-07-12 19:02:31 +0800

    complete test for MapUsingReduce in chapter3

--- src/com/insightfullogic/java8/test/chapter3/MapUsingReduceTest.java
+++ src/com/insightfullogic/java8/test/chapter3/MapUsingReduceTest.java
@@ -1,6 +1,7 @@
 package com.insightfullogic.java8.test.chapter3;
 
 import static org.junit.Assert.*;
+import static java.util.Arrays.asList;
 
 import java.util.Collections;
 import java.util.List;
@@ -11,15 +12,28 @@ import org.junit.Test;
 import com.insightfullogic.java8.answers.chapter3.*;
 
 public class MapUsingReduceTest {
-	
+
 	@Test
-	public void emptyList(){
-		assertMapped(Function.<Object>identity(), Collections.<Object>emptyList(), Collections.<Object>emptyList());
+	public void emptyList() {
+		assertMapped(Function.<Object> identity(), Collections.<Object> emptyList(), Collections.<Object> emptyList());
 	}
 
-	private <I,O> void assertMapped(Function<I,O> mapper,List<I> input,List<O> expectedOutput){
-		List<O> output=MapUsingReduce.map(input.stream(), mapper);
-		assertEquals(expectedOutput, output);
+	@Test
+	public void identityMapsToItself() {
+		assertMapped((Integer x) -> x, asList(1, 2, 3), asList(1, 2, 3));
 	}
-	
+
+	@Test
+	public void incrementingNumbers() {
+		assertMapped((Integer x) -> x + 2, asList(1, 2, 3), asList(3, 4, 5));
+	}
+
+	private <I, O> void assertMapped(Function<I, O> mapper, List<I> input, List<O> expectedOutput) {
+		List<O> output = MapUsingReduce.map(input.stream(), mapper);
+		assertEquals(expectedOutput, output);
+
+		List<O> paralleOutPut = MapUsingReduce.map(input.parallelStream(), mapper);
+		assertEquals(expectedOutput, paralleOutPut);
+	}
+
 }
