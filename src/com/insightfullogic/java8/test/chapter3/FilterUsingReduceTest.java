46f2b45021c805c58be075e5ceca18447b6b7b6a
jxd <jinxd134@foxmail.com>
2016-07-12 19:36:11 +0800

    complete test for FilterUsingReduce in chapter3

--- src/com/insightfullogic/java8/test/chapter3/FilterUsingReduceTest.java
+++ src/com/insightfullogic/java8/test/chapter3/FilterUsingReduceTest.java
@@ -1,7 +1,9 @@
 package com.insightfullogic.java8.test.chapter3;
 
 import static org.junit.Assert.*;
+import static java.util.Arrays.asList;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -12,8 +14,18 @@ import com.insightfullogic.java8.answers.chapter3.FilterUsingReduce;
 public class FilterUsingReduceTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void emptyList() {
+		assertFiltered(x -> false, Collections.<Object> emptyList(), Collections.<Object> emptyList());
+	}
+
+	@Test
+	public void trueReturnsEverything() {
+		assertFiltered((Integer x) -> true, asList(1, 2, 3), asList(1, 2, 3));
+	}
+
+	@Test
+	public void filterPartOfList() {
+		assertFiltered((Integer x) -> x > 2, asList(1, 2, 3), asList(3));
 	}
 
 	private <I> void assertFiltered(Predicate<I> predicate, List<I> input, List<I> exceptedOutput) {
