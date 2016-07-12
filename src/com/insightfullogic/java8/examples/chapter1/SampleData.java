79d89676f6c9f886545c56a6caae66da718bc88c
jxd <jinxd134@foxmail.com>
2016-07-12 12:41:04 +0800

    add sampleData for test

---
+++ src/com/insightfullogic/java8/examples/chapter1/SampleData.java
@@ -0,0 +1,8 @@
+package com.insightfullogic.java8.examples.chapter1;
+
+public class SampleData {
+
+	public static final Artist johnColtrane = new Artist("John Coltrane", "US");
+	public static final Artist johnLennon = new Artist("John Lennon", "UK");
+
+}
