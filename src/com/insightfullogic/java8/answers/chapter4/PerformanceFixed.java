4a3383128ad586f4ed1b97c2bf375b356bd3ec2b
jxd <jinxd134@foxmail.com>
2016-07-13 13:22:32 +0800

    fixed interface Performance in chapter4

--- src/com/insightfullogic/java8/answers/chapter4/PerformanceFixed.java
+++ src/com/insightfullogic/java8/answers/chapter4/PerformanceFixed.java
@@ -1,6 +1,19 @@
 package com.insightfullogic.java8.answers.chapter4;
 
+import java.util.stream.Stream;
+
+import com.insightfullogic.java8.examples.chapter1.Artist;
+
+import static java.util.stream.Stream.concat;
+
 public interface PerformanceFixed {
-	
-	public String getMusicians();
+
+	public String getName();
+
+	public Stream<Artist> getMusicians();
+
+	public default Stream<Artist> getAllMusicians() {
+		return getMusicians()
+				.flatMap(artist -> concat(Stream.of(artist), artist.getMembers()));
+	}
 }
