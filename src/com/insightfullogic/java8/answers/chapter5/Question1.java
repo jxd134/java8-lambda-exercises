2f2770d941e9d55c634bab99f1576779ee68ba5a
jxd <jinxd134@foxmail.com>
2016-07-15 23:34:14 +0800

    add question 1_b in chapter5

--- src/com/insightfullogic/java8/answers/chapter5/Question1.java
+++ src/com/insightfullogic/java8/answers/chapter5/Question1.java
@@ -1,6 +1,7 @@
 package com.insightfullogic.java8.answers.chapter5;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -8,5 +9,9 @@ public class Question1 {
 
 	public List<String> question1_a(List<String> strings) {
 		return strings.stream().map(String::toUpperCase).collect(toList());
+	}
+
+	public int question1_b(List<Integer> integers) {
+		return integers.stream().reduce(0, Integer::sum);
 	}
 }
