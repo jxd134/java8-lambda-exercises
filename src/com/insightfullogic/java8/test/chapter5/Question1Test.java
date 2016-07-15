d8bdbbe2ff316d9c9814cef45fdd61d5c4075a7f
jxd <jinxd134@foxmail.com>
2016-07-16 00:02:39 +0800

    add test for question 1 in chapter5

---
+++ src/com/insightfullogic/java8/test/chapter5/Question1Test.java
@@ -0,0 +1,24 @@
+package com.insightfullogic.java8.test.chapter5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Question1Test {
+
+	@Test
+	public void testQuestion1_a() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQuestion1_b() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQuestion1_c() {
+		fail("Not yet implemented");
+	}
+
+}
