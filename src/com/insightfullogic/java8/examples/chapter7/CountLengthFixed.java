0f65d921447589dd67347d623f700a57650ba945
jxd <jinxd134@foxmail.com>
2016-07-24 01:48:26 +0800

    complete CountLengthFixed class in chapter7

--- src/com/insightfullogic/java8/examples/chapter7/CountLengthFixed.java
+++ src/com/insightfullogic/java8/examples/chapter7/CountLengthFixed.java
@@ -5,11 +5,24 @@ import java.util.function.ToLongFunction;
 
 import com.insightfullogic.java8.examples.chapter1.Album;
 
+// 在CountLength的基础上,合理提取共同点
 public class CountLengthFixed {
-	
+
 	private List<Album> albums;
 
-	public long countFeature(ToLongFunction<Album> function){
+	public long countFeature(ToLongFunction<Album> function) {
 		return albums.stream().mapToLong(function).sum();
+	}
+
+	public long countRunningTime() {
+		return countFeature(album -> album.getTracks().mapToLong(track -> track.getLength()).sum());
+	}
+
+	public long countMusicians() {
+		return countFeature(album -> album.getMusicians().count());
+	}
+
+	public long countTracks() {
+		return countFeature(album -> album.getTracks().count());
 	}
 }
