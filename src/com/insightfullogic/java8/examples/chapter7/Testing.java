fab9193d2cefae6bba5b277335badae0dd58fb84
jxd <jinxd134@foxmail.com>
2016-07-26 00:13:32 +0800

    finish examples in chapter7

--- src/com/insightfullogic/java8/examples/chapter7/Testing.java
+++ src/com/insightfullogic/java8/examples/chapter7/Testing.java
@@ -2,7 +2,6 @@ package com.insightfullogic.java8.examples.chapter7;
 
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
 
 import com.insightfullogic.java8.examples.chapter1.Album;
 import com.insightfullogic.java8.examples.chapter1.Artist;
@@ -13,8 +12,6 @@ import static java.util.stream.Collectors.toList;
 import java.util.HashSet;
 
 public class Testing {
-
-	private static final Logger logger = Logger.getLogger("LambdaTesting");
 
 	public static List<String> allToUpperCase(List<String> words) {
 		return words.stream().map(string -> string.toUpperCase()).collect(toList());
@@ -61,5 +58,14 @@ public class Testing {
 				.map(artist -> artist.getNationality()).collect(toSet());
 		return nationalities;
 	}
-
+	
+	// 使用peek可以在解决方案中添加断点以便调试
+	public static Set<String> nationalityReportUsingPeek(Album album) {
+		Set<String> nationalities = album.getMusicians()
+				.filter(artist -> artist.getName().startsWith("The"))
+				.map(artist -> artist.getNationality())
+				.peek(nation -> System.out.println("Found nationality: " + nation))
+				.collect(toSet());
+		return nationalities;
+	}
 }
